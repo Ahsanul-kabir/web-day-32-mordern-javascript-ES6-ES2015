4cbf19064026da94ede423d934841a93cb2e3058
Ahsanul-Kabir <ahsanul15-8635@diu.edu.bd>
2021-02-03 13:36:20 +0600

    4-Template-string-multipl-line-string added

---
+++ 4-Template-string-multipl-line-string.js
@@ -0,0 +1,20 @@
+const firstName = 'Justin';
+const lastName = 'TimberLake';
+const fullName1 = firstName + ' ' + lastName + 'is a good boy';
+const fullName2 = `${firstName} ${lastName} is a good boy`; //ES6
+console.log(fullName1);
+console.log(fullName2);
+
+
+const taka = `${firstName} tr kaseki ${10+20} taka ase?`; //ES6
+console.log(taka);
+
+const multiLine = 'I love You \n' + 'I heat you \n' + 'your bustrat';
+console.log(multiLine);
+console.log('---------------')
+    // ES6
+const multiLine2 =
+    `I love You
+I heat you
+your bustrat`;
+console.log(multiLine2);
